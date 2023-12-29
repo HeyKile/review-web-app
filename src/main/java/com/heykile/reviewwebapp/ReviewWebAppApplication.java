396c8899e10c9e21033609a4a1b877467da36ffe
HeyKile <bellokp2002@gmail.com>
2023-12-29 13:33:47 -0500

    first commit

---
+++ src/main/java/com/heykile/reviewwebapp/ReviewWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.heykile.reviewwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heykile/reviewwebapp/ReviewWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heykile.reviewwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
